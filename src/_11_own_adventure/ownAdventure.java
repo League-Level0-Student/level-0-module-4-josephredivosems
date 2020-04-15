0c37675089c19f41dfe82de05705aae0515f2340
root <root@44c42443381b>
2020-04-15 23:26:50 +0000

    Hello

--- src/_11_own_adventure/ownAdventure.java
+++ src/_11_own_adventure/ownAdventure.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	int task = JOptionPane.showOptionDialog(null, "You are a prince trying to save a princess stuck inside a castle guarded by dragons. What do you want to do first?", "Choice 1", 0, JOptionPane.INFORMATION_MESSAGE, null,
 			new String[] { "Order your army to create a distraction while you stealthly invade the castle ", "Order your army to kill the dragons with obsidian pointed arrows",}, null);
 	int tasktwo = 0;
+	int taskthree = 5;
 	if(task == 0) {
 	tasktwo = JOptionPane.showOptionDialog(null, "Your army completely gets obliterated by the dragons and you succesfully make it to castle.", "Choice 2", 0, JOptionPane.INFORMATION_MESSAGE, null, new String [] {"Stay in castle forever with princess","Try to climb up dragon's back to escape",}, null);	
 	}
@@ -15,12 +16,26 @@ public static void main(String[] args) {
 	 tasktwo =	JOptionPane.showOptionDialog(null,"You and your army were able to kill 10 dragons and there are only twenty left. Unfortunatley, only a third o yourr army survived" ,"Choice 2", 0, JOptionPane.INFORMATION_MESSAGE, null, new String [] {"Abandon princess at the castle","Make camoflauge out of leaves and steathily make it to the castle",}, null);
 	}
 	if(tasktwo == 0&& task == 0) {
-		JOptionPane.showMessageDialog(null, "The dragons destroy the castle after three days and you and the princess die");
+		JOptionPane.showMessageDialog(null, "The dragons destroy the castle after three days and you and the princess surrender to the dragons");
 		}
 	else if(tasktwo == 1 && task ==  0) {
-		JOptionPane.showOptionDialog(null, "You climb up ", title, optionType, messageType, icon, options, initialValue)
+		 taskthree =JOptionPane.showOptionDialog(null, "You and the princess climn onto the dragon's back. Dragon notices you and takes off flying. You notice a lake below.", "Choice 3", 0, JOptionPane.INFORMATION_MESSAGE, null, new String [] {"Keep flying","Or jump into the lake",}, null);
 	}
-	
+	if(tasktwo == 0 && task == 1) {
+		JOptionPane.showMessageDialog(null, "You abandon the princess at the castle and soon the dragons come back to the kingdom to obliterate it");
+	}
+	else if(tasktwo == 1 && task == 1) {
+		JOptionPane.showMessageDialog(null, "Halfway through the field you realize the dragons are blind and the only use smell so they end you easily");
+	}
+	if(taskthree == 0) {
+		JOptionPane.showMessageDialog(null, "You ride the dragon and all the other dragons follow behind.\n"
+				+ "The dragon you're riding turns its head towards you and shoots a large explosive fireball that kills the rest of \n"
+				+ "the dragons and the only one left is the one you are riding. You get shaken off the dragon and you and the princess \n"
+				+ "fall into the lake. Luckily, the rest of the archers from your army kill the last dragon");
+	}
+	else if(taskthree == 1) {
+		JOptionPane.showMessageDialog(null, "You fal into the lake but the dragons easily end you");
+	}
 	
 	
 	
---
+++ src/_99_extra/checkpoint.java
@@ -0,0 +1,155 @@
+package _99_extra;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class checkpoint {
+public static void main(String[] args) {
+	String favColor = JOptionPane.showInputDialog("What is your favorite color?");
+
+			JOptionPane.showMessageDialog(null, favColor + " is also my favorite color.");
+
+Robot bob = new Robot("mini");
+
+for (int i = 0; i < 3; i++) {
+	bob.penDown();
+	bob.turn(120);
+	bob.move(100);
+}
+
+String favoriteHobby = "programming";
+int numberOfToes = 10;
+int degreesInARightAngle = 90;
+int myAge = 13;
+String programmingStyle = "Java";
+double pi = 3.14159;
+String whatIwantToBeWhenIGrowUp = "famous";
+int facebookFriends = 0;
+String myBirthday = "December 21st 2006";
+int sidesInATriangle = 3;
+String whatIgotForMyBirthday = "projecter";
+String theLastMealIate = "taco";
+String whatTimeIgoToBed = "nine o'clock";
+int howManyRobotProgramsIhaveWritten = 10;
+for (int i = 1; i < 11; i++) {
+	System.out.println(i);
+}
+for (int i = 75; i < 49; i--) {
+	System.out.println(i);
+}
+//a declares the variable
+//b checks condition of variable
+//changes the variables amount
+String yes = "yes";
+if(yes.equals("yes")) {
+	System.out.println("happy");
+}
+else {
+	System.out.println("sad");
+}
+System.out.println("wendesday");
+
+int age = 13;
+if(age < 20) {
+	System.out.println("It's so great being young");
+}
+else {
+	System.out.println("Don't you wish you were young");
+}
+String animalLiked = JOptionPane.showInputDialog("What animal do you like?")
+if(animalLiked.equals("dog")) {
+	JOptionPane.showMessageDialog(null, "dog goes woof");
+}
+else if(animalLiked.equals("cat")) {
+	JOptionPane.showMessageDialog(null, "cat goes meow");
+}
+else if(animalLiked.equals("horse")) {
+	JOptionPane.showMessageDialog(null, "horse goes yeehaw");
+}
+else {
+	JOptionPane.showMessageDialog(null, "I don't know that animal");
+}
+int num = random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
